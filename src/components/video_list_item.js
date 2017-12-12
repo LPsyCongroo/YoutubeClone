6d8eaa65dea55e930a474bc9e40a5401a9304430
Ali Rehmatullah <rehmatullah.ali@gmail.com>
2017-12-12 16:03:28 -0500

    Yay I'm done. Commented out state in SearchBar since it was unnecessary.

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-const VideoListItem = ({video}) => {
+const VideoListItem = ({video, onVideoSelect}) => {
   // const video = props.video;
-  console.log(video);
+  // const onVideoSelect = props.onVideoSelect;
 
-  const imageURL = video.snippet.thumbnails.default.url;
+  const imageUrl = video.snippet.thumbnails.default.url;
 
   return (
-    <li className="list-group-item">
+    <li onClick={() => onVideoSelect(video)} className="list-group-item">
       <div className="video-list-item media">
         <div className="media-left">
-          <img src={imageURL} alt="" className="media-object"/>
+          <img src={imageUrl} alt="" className="media-object"/>
         </div>
         <div className="media-body">
           <div className="media-heading">
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail'; 
 
 const API_KEY = 'AIzaSyBniWc9-sON9gtwMQQUy2hcPu0uZ174qFM';
 
@@ -11,18 +13,35 @@ class App extends Component {
   constructor(props){
     super(props);
 
-    this.state = { videos: [] };
+    this.state = { 
+      videos: [], 
+      selectedVideo: null
+    };
 
+    this.videoSearch('Coke Studio Umair Jaswal');
+  }
+  
+  videoSearch(term) {
     YTSearch( 
-      {key: API_KEY, term: 'coke studio'}, 
-      videos => this.setState({ videos }) 
+      {key: API_KEY, term}, 
+      videos => this.setState({ 
+        videos,
+        selectedVideo: videos[0]
+      }) 
     );
   }
+  
   render(){
+    const videoSearch = _.debounce(term => this.videoSearch(term), 300);
+
     return (
       <div>
-        <SearchBar/>
-        <VideoList videos={this.state.videos} />
+        <SearchBar onSearchTermChange={videoSearch}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList 
+          videos={this.state.videos} 
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+        />
       </div>
     );
   }
